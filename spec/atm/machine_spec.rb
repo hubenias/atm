608f9f3e8dd9d98e736d8ba01deae22654bca7a9
Serhii Hubenia <monoxrom@gmail.com>
2018-03-05 15:56:03 +0200

    Add some specs

--- spec/atm/machine_spec.rb
+++ spec/atm/machine_spec.rb
@@ -4,13 +4,48 @@ describe ATM::Machine do
   subject { described_class.instance }
 
   describe '#deposit' do
-    # proper banknote
-    # error for wrong banknote
-    # + changes avail_banknotes
-    let(:banknotes) { { '1' => 2 } }
+    context 'deposit with proper banknotes' do
+      let(:banknotes) { { '2' => 1, '1' => 2 } }
+      it 'changes available banknotes amount' do
+        subject.deposit(banknotes)
+        expect(subject.banknotes['1']).to eq(2)
+        expect(subject.banknotes['2']).to eq(1)
+      end
+
+      it 'adds to existing banknotes' do
+        subject.banknotes['1'] = 1
+        subject.banknotes['2'] = 1
+        subject.deposit(banknotes)
+        expect(subject.banknotes['1']).to eq(3)
+        expect(subject.banknotes['2']).to eq(2)
+      end
+
+      it 'does not change amount of other banknotes' do
+        subject.banknotes['5'] = 1
+        subject.deposit(banknotes)
+        expect(subject.banknotes['5']).to eq(1)
+      end
+    end
+  end
+
+  describe '#withdrawal' do
+    let(:banknotes) { { '2' => 1, '1' => 2 } }
+    context 'when there is not enough money' do
+      it 'returns nil' do
+        subject.banknotes = banknotes
+        expect(subject.withdrawal(5)).to be_nil
+      end
+    end
+
+    it 'returns proper result' do
+      subject.banknotes = banknotes
+      expect(subject.withdrawal(4)).to eq('2' => 1, '1' => 2)
+    end
+
     it 'changes available banknotes amount' do
-      subject.feed(banknotes)
-      expect(subject.banknotes['1']).to eq(2)
+      subject.banknotes = banknotes
+      subject.withdrawal(3)
+      expect(subject.banknotes).to eq('2' => 0, '1' => 1)
     end
   end
 end
